a124d27b19cb21b62379c3b8cca7847b6f8be1e7
rennieb1 <rennieb1@iinet.net.au>
2019-08-06 12:07:03 +0800

    Main Menu
    Main Menu Build 1

---
+++ Highwynn/Assets/Scripts/MenuScripts/ButtonSelectKeyboard.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ButtonSelectKeyboard : MonoBehaviour
+{
+    
+    public EventSystem eventSystem;
+    public GameObject selectedObject;
+
+    private bool _buttonSelected;
+
+    private void Start()
+    {
+
+    }
+
+    private void Update()
+    {
+        if (Input.GetAxisRaw("Vertical") != 0 && _buttonSelected == false)
+        {
+            eventSystem.SetSelectedGameObject(selectedObject);
+            _buttonSelected = true;
+        }
+    }
+
+    private void OnDisable()
+    {
+        _buttonSelected = false;
+    }
+}
---
+++ Highwynn/Assets/Scripts/MenuScripts/QuitOnClick.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class QuitOnClick : MonoBehaviour
+{
+
+    public void QuickQuit()
+    {
+        Debug.Log ("PLayer has quit");
+        #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+
+    }
+}
