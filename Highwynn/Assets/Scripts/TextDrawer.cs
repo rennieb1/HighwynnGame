a1cbdb320562b706900940535b8179335fb73d44
David Nankivell <David.Nanki@gmail.com>
2019-08-14 22:40:06 +0800

    Add Typewriter Text

---
+++ Highwynn/Assets/Scripts/TextDrawer.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+namespace Highwynn 
+{
+
+    public class TextDrawer : MonoBehaviour
+    {
+        public string outputMessage;
+        public float typeDelay = 0.125f;
+        public Color highlightColour;
+        public TMP_Text outputBox;
+        private bool isTyping = false;
+        private Coroutine typeRoutine;
+
+        void OnTriggerEnter2D(Collider2D other) {
+            if (other.gameObject.tag == "Player" && !isTyping) {
+                typeRoutine = StartCoroutine(TypeText());
+                isTyping = true;
+            }
+        }
+
+        IEnumerator TypeText() {
+            outputBox.text = "";
+            foreach (char c in outputMessage) {
+                if (c == '[') {
+                    outputBox.text += "<#" + ColorUtility.ToHtmlStringRGB(highlightColour) + ">";
+                }
+                else if (c == ']') {
+                    outputBox.text += "</color>";
+                }
+                else {
+                    outputBox.text += c;
+                }
+                yield return new WaitForSeconds(typeDelay);
+            }
+            isTyping = false;
+        }
+
+        void OnTriggerExit2D(Collider2D other) {
+            if (other.gameObject.tag == "Player") {
+                StopCoroutine(typeRoutine);
+                outputBox.text = "";
+                isTyping = false;
+            }
+        }
+    }
+
+}
+
