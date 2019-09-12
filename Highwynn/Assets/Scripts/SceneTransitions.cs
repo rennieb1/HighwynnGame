e051285391eadcebb96ed332554627722a6b9b1d
rennieb1 <rennieb1@iinet.net.au>
2019-09-12 23:43:08 +0800

    Pallet update, unfinished split levels
    created a bunch of tiles, to make the level feel more, created a new folder for the levels which will be implemented into the main file once they have the correct assets.

---
+++ Highwynn/Assets/Scripts/SceneTransitions.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneTransitions : MonoBehaviour
+{
+public Animator transitionAnim;
+public string sceneName;
+
+
+void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            StartCoroutine (LoadScene());
+        }
+    }
+
+    IEnumerator LoadScene ()
+    {
+        transitionAnim.SetTrigger("transitionexit");
+        yield return new WaitForSeconds (1.5f);
+        SceneManager.LoadScene(sceneName);
+    }
+}
