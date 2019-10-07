1c8bb3810ee37fe09f754fa0247682515943d806
rennieb1 <rennieb1@iinet.net.au>
2019-10-07 18:48:11 +0800

    Stag Enemy hooked up, will require tweaking by David though
    Updated the code
    Hooked up the animations.

--- Highwynn/Assets/Scripts/EnemyBehaviour/StagBehaviour.cs
+++ Highwynn/Assets/Scripts/EnemyBehaviour/StagBehaviour.cs
@@ -13,17 +13,37 @@ namespace Highwynn
             string name = anim.GetCurrentAnimatorClipInfo(0)[0].clip.name;
 
             if (other.tag == "Player") {
+                if (name == "Stag_Idle" ||  name == "Walk_Start")
+                {
+                    anim.Play("Attack_Charge");
+                    anim.ResetTrigger("idleWalk");
+                    anim.ResetTrigger("walkIdle");
+                }
+
                 // IF IDLE ANIMATION
                 // THEN RUN OR ATTACK ANIMATION
                 // DEPENDING ON DISTANCE
                 // AND STYLE OF ENEMY ATTACK
+                anim.SetBool("lostPlayer", false);
             }
 
             if (other.gameObject.layer == 13) {
                 // IF TURNAROUND IS [DISTANCE] AWAY
+                if (distance < shortViewDistance)
+                {
+                    stopped = true;
+                    if (name == "Stag_Idle" || name == "Walk_Start")
+                    {
+                        movingRight = !movingRight;
+                        Flip();
+                        stopped = false;
+                    }
+                    else if (name == "Attack_Charge" || name == "Walk_End")
+                    {
+                        Stop();
+                    }
 
-                // movingRight = !movingRight;
-                // Flip();
+                }
             }
         }
 
@@ -32,27 +52,100 @@ namespace Highwynn
             string name = anim.GetCurrentAnimatorClipInfo(0)[0].clip.name;
 
             switch(name) {
+
                 // case "ANIMATION_NAME":
                 //      SET SPEED
                 //      break;
+
+                case "Walk_Start":
+                    Move(1.0f);
+                    break;
+
+                case "Walk_Continuous":
+                    Move(3.0f);
+                    break;
+
+                case "Attack_Charge":
+                    anim.SetBool("seePlayer", false);
+                    Move(8.0f);
+                    break;
+                case "Walk_End":
+                    Move(1.0f);
+                    break;
+                case "Stag_Idle":
+                    Move(0.0f);
+                    break;
+                case "Stag_Death":
+                    Move(0.0f);
+                    break;
+
+                   
             }
 
-            Move(speed);
+           
 
             // PERHAPS USE TIMER HERE TO HANDLE IDLE LENGTH
         }
 
         protected override void PlayerLost() {
+            if (!anim.GetBool("lostPlayer"))
+            {
+                anim.SetBool("lostPlayer", true);
+            }
             // PLAY IDLE ANIMATION
             // ~ OR TRANSITION TO ~
         }
 
         protected override void OnDeath() {
             dying = true;
-            speed = 0.0f;
+           speed = 0.0f;
             // PLAY DEATH ANIMATION
+            anim.Play("Stag_Death");
+        }
+        protected override void Stop()
+        {
+            anim.Play("Stag_Idle");
+        }
+        void OnStopped()
+        {
+            if (stopped)
+            {
+                movingRight = !movingRight;
+                Flip();
+            }
+
+            stopped = false;
         }
 
+        private void DecideDirection()
+        {
+            movingRight = Random.value >= 0.5f;
+            // Flip is a base-class function
+            Flip();
+        }
+
+        // Used by EndIdle to determine if moving or idling
+        private void DecideMove()
+        {
+            bool move = (Random.value >= 0.5f);
+            if (move)
+            {
+                anim.SetBool("idleWalk", true);
+                anim.SetBool("walkIdle", false);
+            }
+            else
+            {
+                anim.SetBool("walkIdle", true);
+                anim.SetBool("idleWalk", false);
+            }
+        }
+        void OnColliderEnter2D(Collider2D other)
+        {
+            if (other.gameObject.tag == "EnemyTurnaround")
+            {
+                Stop();
+            }
+        }
         //////////////////////////////////////////////////// Animation Events /////////////////////////////////////////////////////
         // EACH OF THESE SUGGESTIONS IS DEPENDANT ON WHAT ANIMATIONS STAG HAS
 
