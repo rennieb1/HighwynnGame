50d4969cbf5d742931e8d91e9a1ebd7f2fcfe2f0
rennieb1 <49140451+rennieb1@users.noreply.github.com>
2019-10-21 11:20:35 +0800

    Added auto transition to the final page

---
+++ Highwynn/Assets/Scripts/EndLoader.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndLoader : MonoBehaviour
+{
+public string sceneName;
+
+    void Start()
+    {
+        StartCoroutine (LoadScene());    
+    }
+
+    IEnumerator LoadScene ()
+    {       
+        yield return new WaitForSeconds (1.5f);
+        SceneManager.LoadScene(sceneName);
+    }
+}
