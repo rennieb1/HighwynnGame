d006ee2d3d1703b6cc5afccf592f15d7f22ef896
David Nankivell <David.Nanki@gmail.com>
2019-11-14 15:10:54 +0800

    Add New Mouse Cursor

---
+++ Highwynn/Assets/Scripts/MenuScripts/MouseInit.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseInit : MonoBehaviour
+{
+    [SerializeField]
+    private Texture2D cursorTexture;
+    [SerializeField]
+    private CursorMode cursorMode = CursorMode.Auto;
+    [SerializeField]
+    private Vector2 hotspot = Vector2.zero;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.SetCursor(cursorTexture, hotspot, cursorMode);
+    }
+}
